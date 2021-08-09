89d83892496d1fe5ee1380ffe92406766baacc45
osmar-pj <osmar.pj@hotmail.com>
2021-08-09 13:37:45 -0500

    datos procesados

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -6,14 +6,38 @@ export const getDataByDates = async (req, res) => {
             first: new Date(req.body.dates.first),
             last: new Date(req.body.dates.last)
         }
+        let data_esp = {
+            t: [],
+            h: [],
+            v: [],
+            I: [],
+            P: [],
+            date: []
+        }
+        let data_rasp = {
+            wind: [],
+            angle: [],
+            date: []
+        }
         const esp32 = await House.find({mac: '24:0A:C4:16:72:00', createdAt: { $gt: dates.first, $lt: dates.last } })
-    
+        for (let i = 0; i < esp32.length; i++) {
+            data_esp.t[i] = parseFloat(esp32[i].values.t)
+            data_esp.h[i] = parseFloat(esp32[i].values.h)
+            data_esp.v[i] = parseFloat(esp32[i].values.v)
+            data_esp.I[i] = parseFloat(esp32[i].values.I)
+            data_esp.P[i] = parseFloat((data_esp.v[i] * data_esp.I[i]).toFixed(2))
+            data_esp.date[i] = esp32[i].createdAt
+        }
         const raspberry = await House.find({mac: 'raspberry', createdAt: { $gt: dates.first, $lt: dates.last } })
-
+        for (let i = 0; i < raspberry.length; i++) {
+            data_rasp.wind[i] = parseFloat(raspberry[i].values[15])
+            data_rasp.angle[i] = parseFloat(raspberry[i].values[17])
+            data_rasp.date[i] = raspberry[i].createdAt
+        }
         // console.log(esp32, raspberry)
         res.status(200).json({
-            esp32: esp32,
-            raspberry: raspberry,
+            data_esp,
+            data_rasp
         })
     } catch (error) {
         console.error(error)
